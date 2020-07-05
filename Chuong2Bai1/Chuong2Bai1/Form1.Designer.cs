a8809c7324eb9614372676ca59782564db368dca
NguyenLuongDuyKhanh <16520584@gm.uit.edu.vn>
2020-07-05 23:42:16 +0700

    Backup course project

---
+++ Chuong2Bai1/Chuong2Bai1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Thực thi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 38);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Kết nối";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(690, 313);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(714, 465);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SSH Session";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Chuong2Bai1/Chuong2Bai1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //Connection information
+        public string user = "khanh";
+        string pass = "khanh2019";
+        string host = "192.168.1.8";
+        bool connect_status;
+        //int port = 22;
+        public SshClient client;
+        private ShellStream shell;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "khanh";
+            textBox5.Text = "khanh2019";
+            textBox4.Text = "192.168.1.8";
+            textBox3.Enabled = false;
+            textBox3.ReadOnly = true;
+            button1.Enabled = false;
+            connect_status = false;
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.AppendText(client.RunCommand(textBox3.Text).Result);
+                richTextBox1.AppendText(">\n");
+                textBox3.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                try
+                {
+                    client.Connect();
+                }
+                catch { }
+            }
+
+        }
+
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            if(!connect_status)
+            {
+                connect_status = true;
+                user = textBox1.Text;
+                pass = textBox5.Text;
+                host = textBox4.Text;
+                client = new SshClient(host, user, pass);
+                try
+                {
+                    client.Connect();// SshAuthenticationException
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                textBox1.Enabled = false;
+                textBox1.ReadOnly = true;
+                textBox5.Enabled = false;
+                textBox5.ReadOnly = true;
+                textBox4.Enabled = false;
+                textBox4.ReadOnly = true;
+                textBox3.Enabled = true;
+                textBox3.ReadOnly = false;
+                button1.Enabled = true;
+                button2.Text = "Ngắt kết nối";
+                
+            }else
+            if(connect_status)
+            {
+                connect_status = false;
+                try
+                {
+                    client.Disconnect();
+                }
+                catch(Exception a)
+                {
+                    return;
+                }
+                textBox1.Enabled = true;
+                textBox1.ReadOnly = false;
+                textBox5.Enabled = true;
+                textBox5.ReadOnly = false;
+                textBox4.Enabled = true;
+                textBox4.ReadOnly = false;
+                textBox3.Enabled = false;
+                textBox3.ReadOnly = true;
+                button1.Enabled = false;
+                button2.Text = "Kết nối";
+           
+            }       
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Enter))
+            {
+                try
+                {
+                    richTextBox1.AppendText(client.RunCommand(textBox3.Text).Result);
+                    richTextBox1.AppendText(">\n");
+                    textBox3.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    try
+                    {
+                        client.Connect();
+                    }
+                    catch { }
+                }
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
